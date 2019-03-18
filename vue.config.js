d59d9f9ad80ae3bbb2082916ea8179b5dd40a841
Chris Puzzuoli <cpuzzuol@umich.edu>
2019-03-18 18:04:43 -0400

    added config file

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// module.exports = {
+// 	baseUrl: process.env.NODE_ENV === 'production'
+// 		? '/' // asset url prefix in production mode
+// 		: '/', // asset url prefix in development mode
+// 	outputDir: '/Users/cpuzzuol/mampstack/apache2/htdocs/ucrisko', // running 'yarn build' will send production code here
+// }
