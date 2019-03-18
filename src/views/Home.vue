575ab6a4ff6c7ec065f2349946d37cbe309f9ea0
Chris Puzzuoli <cpuzzuol@umich.edu>
2019-03-18 17:55:46 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img height="500" width="500" alt="University of Crisko logo" src="../assets/uclogo.png">
+    <UCLanding msg="The University of CRISKO."/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import UCLanding from '@/components/UCLanding.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    UCLanding
   }
 }
 </script>
