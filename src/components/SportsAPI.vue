860636826d2e7cfb7b42870e1d54a46a2d502284
emulibrary <emulibrary@gmail.com>
2019-04-21 22:18:39 -0400

    sports api

---
+++ src/components/SportsAPI.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Sports API</h1>
+    <button type="button" @click="fetchStandings">Get sports API data</button><br>
+    <div>{{ standings }}</div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: "sportsapi",
+    data() {
+      return {
+        standings: [],
+      }
+    },
+    methods: {
+      fetchStandings(){
+        axios.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2018-regular/overall_team_standings.json',
+          {
+            auth: {
+              username: 'c5fa253b-b758-4a63-b92f-7edc6e',
+              password: '4i)JhNC6bdn2Gj'
+            }
+          }
+        )
+          .then(response => {
+            this.standings = response.data
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      }
+    }
+  }
+</script>
+
+
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import SubmissionSuccess from './components/SubmissionSuccess'
 import SubmissionFail from './components/SubmissionFail'
+import SportsAPI from './components/SportsAPI'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ export default new Router({
       path: '/404',
       name: 'fail',
       component: SubmissionFail
+    },
+    {
+      path: '/sports',
+      name: 'sportsapi',
+      component: SportsAPI
     }
   ]
 })
